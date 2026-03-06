ceb65d06d8b9a1f374bd41a5d38da8fd3a737870
Łukasz Szewczyk <szewczyk134@gmail.com>
2026-03-06 16:27:07 +0100

    fix: update greeting message in server-axum for consistency

--- apps/server-axum/src/main.rs
+++ apps/server-axum/src/main.rs
@@ -6,7 +6,7 @@ use axum::{
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(|| async { "Hello, World!!!" }));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
